e48ee282f85b4c4aecaed227cc59e273c64287c6
Vj <venkatesh.jatla@gmail.com>
2017-11-18 09:02:07 -0700

    Initial commit having directory sturcture

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='Drastic',
+      version='0.1',
+      description='Real-time adaptive software for video processing',
+      url='http://github.com/nuzen/DRASTIC-Soft',
+      author='Venkatesh Jatla (or) Nuzen',
+      author_email='venkatesh.jatla@gmail.com',
+      license='???',
+      packages=['DRASTIC-Soft'],
+      zip_safe=False)
